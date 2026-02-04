437bd4e9b044a8b93aee1a850ca4b700d1e54024
Mozammal Hossain <mozammal.hossain.dev@gmail.com>
2026-02-04 15:47:53 +0600

    feat: Implement repository

--- Services/PatientRepository.cs
+++ Services/PatientRepository.cs
@@ -1,28 +1,59 @@
+
 class PatientRepository : IPatientRepository
 {
+    private readonly Dictionary<int, Patient> _patients = new();
+    private int _nextId = 1;
 
     public Patient? Add(Patient patient)
     {
-        throw new NotImplementedException();
+        patient.Id = _nextId++;
+        _patients.Add(patient.Id, patient);
+        return patient;
     }
 
     public bool Delete(int id)
     {
-        throw new NotImplementedException();
+        Patient? patient = GetById(id);
+
+        if (patient == null)
+        {
+            return false;
+        }
+        else
+        {
+            _patients.Remove(id);
+            return true;
+        }
+
     }
 
     public IEnumerable<Patient> GetAll()
     {
-        throw new NotImplementedException();
+        return _patients.Values;
     }
 
     public Patient? GetById(int id)
     {
-        throw new NotImplementedException();
+        return _patients.TryGetValue(id, out var _) ? _patients[id] : null;
     }
 
     public Patient? Update(Patient patient)
     {
-        throw new NotImplementedException();
+        Patient? existingPatient = GetById(patient.Id);
+        if (existingPatient == null)
+        {
+            return null;
+        }
+        existingPatient.FullName = patient.FullName;
+        existingPatient.DateOfBirth = patient.DateOfBirth;
+        existingPatient.Gender = patient.Gender;
+        existingPatient.PhoneNumber = patient.PhoneNumber;
+        existingPatient.Email = patient.Email;
+        existingPatient.AdmittedAt = patient.AdmittedAt;
+        existingPatient.IsDischarged = patient.IsDischarged;
+        _patients[existingPatient.Id] = existingPatient;
+        return existingPatient;
     }
+
 }
+
