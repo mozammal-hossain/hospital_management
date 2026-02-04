9230625025873061cf16480fcce212d393fca083
Mozammal Hossain <mozammal.hossain.dev@gmail.com>
2026-02-04 23:25:33 +0600

    feat: Implement logger and validation

--- Models/CreatePatientRequest.cs
+++ Models/CreatePatientRequest.cs
@@ -1,8 +1,23 @@
+using System.ComponentModel.DataAnnotations;
 public class CreatePatientRequest
 {
+    [Required(ErrorMessage = "FullName is required")]
+    [StringLength(400, MinimumLength = 1, ErrorMessage = "FullName must be between 1 and 400 characters")]
     public required string FullName { get; set; }
+
+    [Required(ErrorMessage = "DateOfBirth is required")]
     public required DateTime DateOfBirth { set; get; }
+
+    [Required(ErrorMessage = "Gender is required")]
+    [StringLength(10, MinimumLength = 1, ErrorMessage = "Gender must be between 1 and 10 characters")]
     public required string Gender { get; set; }
+
+    [Required(ErrorMessage = "PhoneNumber is required")]
+    [RegularExpression(@"^01[3-9]\d{8}$", ErrorMessage = "Invalid phone number")]
+    [StringLength(11, ErrorMessage = "PhoneNumber must be 11 digits")]
     public required string PhoneNumber { get; set; }
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email address")]
     public required string Email { get; set; }
 }
--- Models/UpdatePatientRequest.cs
+++ Models/UpdatePatientRequest.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 public class UpdatePatientRequest
 {
+
+    [StringLength(400, MinimumLength = 1, ErrorMessage = "FullName must be between 1 and 400 characters")]
     public string? FullName { get; set; }
     public DateTime? DateOfBirth { get; set; }
+
+    [StringLength(10, MinimumLength = 1, ErrorMessage = "Gender must be between 1 and 10 characters")]
     public string? Gender { get; set; }
+
+    [RegularExpression(@"^01[3-9]\d{8}$", ErrorMessage = "Invalid phone number")]
+    [StringLength(11, ErrorMessage = "PhoneNumber must be 11 digits")]
     public string? PhoneNumber { get; set; }
+
+    [EmailAddress(ErrorMessage = "Invalid email address")]
     public string? Email { get; set; }
     public DateTime? AdmittedAt { get; set; }
     public bool? IsDischarged { get; set; }
